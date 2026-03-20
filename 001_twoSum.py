82c307d03b495d809066a5c589f89dc718342177
hml <930005969@qq.com>
2026-03-20 10:38:20 +0800

    revise 001

---
+++ 001_twoSum.py
@@ -0,0 +1,37 @@
+# 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+# 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+# 你可以按任意顺序返回答案
+# 输入：nums = [2,7,11,15], target = 9
+# 输出：[0,1]
+# 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+from typing import List
+
+class Solution:
+    def twoSum_On2(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+    def twoSum_On1(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return []
+
+def main():
+    solution = Solution()
+    nums = [15,7,11,2]
+    target = 9
+    result = solution.twoSum_On2(nums, target)
+    result_On1 = solution.twoSum_On1(nums, target)
+    print(f"Output: {result}")
+    print(f"Output: {result_On1}")
+
+if __name__ == "__main__":
+    main()
